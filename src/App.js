16e991033daa1dd0f4dda94ae8c06afb8c0c3607
gonjila <lgonjila@gmail.com>
2022-02-07 03:15:05 +0400

    made markup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const onFormSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="formWrapper">
+        <h1>React Form</h1>
+        <form onSubmit={onFormSubmit}>
+          <input name="text" type="text" placeholder="Name" />
+          <input name="email" type="email" placeholder="Email" />
+          <select name="select" id="browsers">
+            <option value="Edge">Edge</option>
+            <option value="Firefox" selected>
+              Firefox
+            </option>
+            <option value="Chrome">Chrome</option>
+            <option value="Opera">Opera</option>
+            <option value="Safari">Safari</option>
+          </select>
+          <input name="number" type="number" placeholder="Age" />
+
+          <button type="submit" disabled={false}>
+            click me
+          </button>
+        </form>
+        <div className="errorWrapper">{null}</div>
+      </div>
     </div>
   );
 }
